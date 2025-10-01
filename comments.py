f40fc1a2dda15f2e6e81b9170d394f80a2d4539c
jade9890 <jadeluong789@gmail.com>
2025-10-01 19:13:08 +0100

    first attempt

---
+++ comments.py
@@ -0,0 +1,6 @@
+#here's a comment
+print ("this is not a comment")
+print ("to make a comment, use \# ...")
+print ("or write in a string literal (triple quotes) ")
+"""like this lol"""
+#here's another comment
